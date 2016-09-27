a38deb991c3d800e90b4c11969bce5bc80bb6078
Dobin Rutishauser <dobin@broken.ch>
2016-09-27 07:25:48 +0200

    updatecontainer update

--- adminscripts/updatecontainer.sh
+++ adminscripts/updatecontainer.sh
@@ -7,9 +7,11 @@ echo "Update: hlUbuntu32 and hlUbuntu64"
 lxc start hlUbuntu32
 lxc start hlUbuntu64
 
-sleep 1
+sleep 3
 
+echo "Ubuntu32: "
 lxc exec hlUbuntu32 -- /bin/bash -c 'cd /root/challenges; git pull;'
+echo "Ubuntu64: "
 lxc exec hlUbuntu64 -- /bin/bash -c 'cd /root/challenges; git pull;'
 
 sleep 1
