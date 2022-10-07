01f53b283f5a44b9da0c60497366468f2da9a61c
Dobin Rutishauser <garbage@broken.ch>
2022-10-07 13:53:50 +0000

    fix for sudo

--- adminscripts/fw.sh
+++ adminscripts/fw.sh
@@ -2,11 +2,11 @@
 
 if [ "$1" == "enable" ]; then
         echo "Enable Container Firewall"
-        iptables -A FORWARD -j DROP
+        sudo /sbin/iptables -A FORWARD -j DROP
 fi
 
 if [ "$1" = "disable" ]; then
         echo "Disable Firewall"
-        iptables -D FORWARD -j DROP
+        sudo /sbin/iptables -D FORWARD -j DROP
 fi
 
